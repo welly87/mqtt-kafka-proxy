23319639eec05e63186028e0428e3a99e300d5b4
Welly Tambunan <if05041@gmail.com>
2020-01-13 08:30:32 +0700

    add proxy mqtt to kafka

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+func connect(clientId string, uri *url.URL) mqtt.Client {
+	opts := createClientOptions(clientId, uri)
+	client := mqtt.NewClient(opts)
+	token := client.Connect()
+	for !token.WaitTimeout(3 * time.Second) {
+	}
+	if err := token.Error(); err != nil {
+		log.Fatal(err)
+	}
+	return client
+}
+
+func createClientOptions(clientId string, uri *url.URL) *mqtt.ClientOptions {
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("ws://%s", uri.Host))
+	opts.SetUsername(uri.User.Username())
+	password, _ := uri.User.Password()
+	opts.SetPassword(password)
+	opts.SetClientID(clientId)
+	return opts
+}
+
+func listen(uri *url.URL, topic string) {
+	client := connect("sub", uri)
+	client.Subscribe(topic, 0, func(client mqtt.Client, msg mqtt.Message) {
+		fmt.Printf("* [%s] %s\n", msg.Topic(), string(msg.Payload()))
+	})
+}
+
+func main() {
+	uri, err := url.Parse("tcp://localhost:1883")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	topic := "something"
+
+	choke := make(chan [2]string)
+
+	listen(uri, topic)
+
+	<-choke
+}
